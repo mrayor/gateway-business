acd083042c858a8ed48749ee49e2ab27e14127ba
Adesanya Ayokunle <adesanyaayokunle@gmail.com>
2019-10-18 11:08:18 +0100

    edited users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('is_admin')->default(0);
+            $table->unsignedInteger('listing_id')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
